f2d32a24223affa471c17e585750af06feda5a3c
Bartosz Pazdur <b.pazdur@onet.pl>
2021-07-25 14:23:35 +0200

    New template file
    Create new template

--- pages/posts/automatyzacja_zycia.vue
+++ pages/posts/automatyzacja_zycia.vue
@@ -1,50 +1,17 @@
 <template lang="pug">
-  section.wrapper(aria-label="Zawartość postu" aria-labelledby="content-wrapper")
-    <Nav title="#StaySecure" />
-    div.container#content-wrapper
-
-      .content
-        nav(aria-label="breadcrumb")
-          ol.breadcrumb
-            li.breadcrumb-item
-              a(href="/") start
-            li.breadcrumb-item
-              a(href="/posts") wpisy
-            li.breadcrumb-item.active
-              | {{ getCurrentPage }}
-        include:markdown-it ./posts_dir/automatyzacja-zycia.md
-    footer.footer-content
-      <Footer />
+  <core>
+    include:markdown-it ./posts_dir/automatyzacja-zycia.md
+  </core>
 </template>
 
 <script>
-import Nav from "@/components/Nav.vue";
-import Footer from "@/components/Footer/Footer.vue";
-
+import Core from './templates/core.template.vue'
 export default {
   components: {
-    Nav,
-    Footer
-  },
-  computed: {
-    getCurrentPage() {
-      return this.$router.history.current.name
-    }
+    Core,
   }
 };
 </script>
 
-<style lang="scss" scoped>
-@import "~/node_modules/bootstrap/scss/bootstrap";
-@import "./single_posts.styles.scss";
-@import "~/assets/scss/colors.scss";
-
-.content {
-  padding: 40px 0;
-  font-family: Poppins, sans-serif;
-  width: 100%;
-  margin: 0 auto;
-  font-smoothing: antialiased;
-  -webkit-font-smoothing: antialiased;
-}
+<style>
 </style>
---
+++ pages/posts/templates/core.template.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  section.wrapper(aria-label="Zawartość postu" aria-labelledby="content-wrapper")
+    <Nav title="#StaySecure" />
+    div.container#content-wrapper
+      .content
+        nav(aria-label="breadcrumb")
+          ol.breadcrumb
+            li.breadcrumb-item
+              a(href="/") start
+            li.breadcrumb-item
+              a(href="/posts") wpisy
+            li.breadcrumb-item.active
+              | {{ getCurrentPage }}
+        <slot></slot>
+    footer.footer-content
+      <Footer />
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+import Footer from "@/components/Footer/Footer.vue";
+
+export default {
+  components: {
+    Nav,
+    Footer
+  },
+  computed: {
+    getCurrentPage() {
+      return this.$router.history.current.name;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/node_modules/bootstrap/scss/bootstrap";
+@import "../single_posts.styles.scss";
+@import "~/assets/scss/colors.scss";
+
+.content {
+  padding: 40px 0;
+  font-family: Poppins, sans-serif;
+  width: 100%;
+  margin: 0 auto;
+  font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
