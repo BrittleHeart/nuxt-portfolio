540e293e6c2e61ba7d87254b71f0148728e89ec0
Bartosz Pazdur <b.pazdur@onet.pl>
2021-07-26 17:49:07 +0200

    Clean
    Clean useless code

--- components/Main/posts/best-posts.ts
+++ components/Main/posts/best-posts.ts
@@ -9,22 +9,4 @@ export const best_posts: IPost[] = [
     created_at: "10 Lipiec",
     description: `Dlaczego automatyzacja życia w dzisiejszych czasach to must have ?`,
   },
-  {
-    id: 2,
-		name: 'bezpieczenstwo_maili',
-    title: "Bezpieczeństwo maili",
-    badge: "Jak zadbać o bezpieczeństwo skrzynki pocztowej?",
-    created_at: "18 Czerwiec",
-    description: `W jaki sposób chronić swoje wiadomości mailowe 
-              oraz swoje prywatne dane przed nieproszonymi goścmi?`,
-  },
-  {
-    id: 3,
-		name: 'pierwsza_pomoc',
-    title: "Pierwsza pomoc",
-    badge: "Udzielanie pierwszej pomocy osobie potrzebującej",
-    created_at: "25 Kwiecień",
-    description: `W jaki spobób udzielić pierwszej pomocy osobie po wypadku samochodowym? 
-                W jaki sposób uratować życia Diabetykowi?`,
-  }
 ];
